72a970f0d5c323ac1f101adf68f5da58fb36a59a
Juan C <juanconrod@protonmail.com>
2025-02-26 20:02:00 +0100

    `FuncInterface.lean`: add results for `«#sizeWordStack»` and more

--- EvmEquivalence/Interfaces/FuncInterface.lean
+++ EvmEquivalence/Interfaces/FuncInterface.lean
@@ -5,8 +5,10 @@ Some of the contents of this file may be ported to a utils file in the future.
 
 import EvmYul.UInt256
 import EvmEquivalence.KEVM2Lean.Func
+import EvmEquivalence.StateMap
 
 open EvmYul
+open StateMap
 
 namespace KEVMInterface
 
@@ -40,10 +42,14 @@ def «_*Int'_» (n m : SortInt) : SortInt :=
 theorem mulInt_def (n m : SortInt) : «_*Int'_» n m = n * m := by rfl
 
 -- Behavior for `chop`
-axiom chopIsSome : ∀ n : SortInt, chop n = some (n % UInt256.size)
+--`_modInt_` is still uninterpreted
+theorem chopIsSome (n : SortInt) : chop n = some (n % UInt256.size) := by sorry
 
 noncomputable def chop' (n : SortInt) : SortInt :=
   (chop n).get (by simp [chopIsSome])
+
+theorem chop_def (n : SortInt) : chop' n = n % UInt256.size := by
+  simp [chop', Option.get]; split; rename_i _ _ _ _ eq _; simp_all [chopIsSome]
 
 -- Utils to aid in proofs
 def SortGas.val (g : SortGas) : SortInt :=
@@ -51,6 +57,9 @@ def SortGas.val (g : SortGas) : SortInt :=
 
 theorem subGas_def (g₁ g₂ : SortGas) :
   «_-Gas__GAS-SYNTAX_Gas_Gas_Gas»  g₁ g₂ = SortGas.inj_SortInt («_-Int'_» (SortGas.val g₁) (SortGas.val g₂)) := by rfl
+
+theorem leGas_def (g₁ g₂ : SortGas) :
+  «_<=Gas__GAS-SYNTAX_Bool_Gas_Gas»  g₁ g₂ = «_<=Int_» (SortGas.val g₁) (SortGas.val g₂) := by rfl
 
 -- Behavior of `«_==K_»` and `«_=/=K_»`
 axiom Keq_def (k₁ k₂ : SortK) : «_==K_» k₁ k₂ = some (k₁ == k₂)
@@ -60,5 +69,57 @@ axiom Kneq_def (k₁ k₂ : SortK) : «_=/=K_» k₁ k₂ = some (k₁ != k₂)
 axiom orBool_def (b₁ b₂ : SortBool) : _orBool_ b₁ b₂ = some (b₁ || b₂)
 axiom andBool_def (b₁ b₂ : SortBool) : _andBool_ b₁ b₂ = some (b₁ && b₂)
 axiom notBool_def (b : SortBool) : notBool_ b = some (!b)
+
+-- Behavior of `«#sizeWordStack»`
+-- Reasoning-friendly #sizeWordStack
+def wsLength (ws : SortWordStack) : ℕ :=
+  match ws with
+  | .«.WordStack_EVM-TYPES_WordStack» => 0
+  | .«_:__EVM-TYPES_WordStack_Int_WordStack» _ ws => 1 + wsLength ws
+
+theorem sizeWordStackNotEmpty (n w : SortInt) (ws : SortWordStack) :
+  sizeWordStackAux (.«_:__EVM-TYPES_WordStack_Int_WordStack» w ws) n =
+  sizeWordStackAux ws (n + 1) := by
+  simp [sizeWordStackAux, _432555e, _75897fa, plusIntIsSome]
+
+theorem sizeWordStack_add_one (w : SortInt) (ws : SortWordStack) :
+  ∀ n, sizeWordStackAux (.«_:__EVM-TYPES_WordStack_Int_WordStack» w ws) n = do
+  let val0 ← sizeWordStackAux ws n
+  let val1 ← «_+Int_» 1 val0
+  return val1 := by
+  induction ws <;> simp_all [sizeWordStackAux, _432555e, _75897fa, plusIntIsSome, Int.add_comm]
+
+theorem sizeWordStackAuxAdd (n : SortInt) (n_nonneg: 0 ≤ n) :
+  ∀ ws, sizeWordStackAux ws (n + 1) = do
+  let a ← sizeWordStackAux ws n
+  let b ← «_+Int_» 1 a
+  return b := by
+  cases n <;> try contradiction
+  rename_i n; cases n
+  . intro ws
+    rw [←(sizeWordStackNotEmpty (Int.ofNat 0))]
+    rw [(sizeWordStack_add_one _ ws (Int.ofNat 0))]; exact 0
+  . rename_i n ;intro ws
+    rw [←(sizeWordStackNotEmpty (Int.ofNat (n + 1)))]
+    rw [(sizeWordStack_add_one _ ws (Int.ofNat (n + 1)))]; exact 0
+
+theorem sizeWordStackIsSome (ws : SortWordStack) :
+  «#sizeWordStack» ws = some (wsLength ws) := by
+  induction ws with
+  | «.WordStack_EVM-TYPES_WordStack» =>
+    simp [wsLength, «#sizeWordStack», _c0f9e27, sizeWordStackAux, _432555e, _75897fa]
+  | «_:__EVM-TYPES_WordStack_Int_WordStack» w wss ih =>
+    rw [«#sizeWordStack», _c0f9e27] at *
+    simp_all only [Option.pure_def, Option.bind_eq_bind, Option.bind_some]
+    rw [sizeWordStack_add_one, ih]
+    aesop
+
+theorem wsLength_def (ws : SortWordStack) :
+  wsLength ws = List.length (wordStackMap ws) := by
+  induction ws <;> simp_all [wsLength, wordStackMap, Nat.add_comm]
+
+theorem sizeWordStack_def (ws : SortWordStack) :
+  «#sizeWordStack» ws = some (List.length (wordStackMap ws)) := by
+  rw [sizeWordStackIsSome, wsLength_def]
 
 end KEVMInterface
