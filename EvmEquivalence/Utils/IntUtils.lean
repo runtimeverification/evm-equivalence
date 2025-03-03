9e28144b779627db45156b8843fd7975463d878b
Juan C <juanconrod@protonmail.com>
2025-03-03 17:55:01 +0100

    `IntUtils.lean`: remove theorem `le_fls_of_lt` and unnecessary import

--- EvmEquivalence/Utils/IntUtils.lean
+++ EvmEquivalence/Utils/IntUtils.lean
@@ -1,8 +1,5 @@
-import Mathlib.Order.Defs.LinearOrder
 import Mathlib.Order.Lattice
 import Aesop
-
-theorem le_fls_of_lt {n m : Nat} (h : n < m) : (m ≤ n) = False := by aesop
 
 theorem Int.mod_cast {n m : Int} (le0n: 0 ≤ n) (le0m : 0 ≤ m):
   n % m = ↑((Int.toNat n) % (Int.toNat m)) := by aesop
