4c5fdddecd3f05ce2d33d1545d3e919dc6995d5a
Juan C <juanconrod@protonmail.com>
2025-03-21 12:32:00 +0100

    `SstoreSummary.lean`: prove `EvmYul.step_sstore_summary`

--- EvmEquivalence/Summaries/SstoreSummary.lean
+++ EvmEquivalence/Summaries/SstoreSummary.lean
@@ -40,60 +40,47 @@ theorem sstore_bypass_private {symState : EVM.State}:
   EvmYul.step_sstore symState =
   EVM.binaryStateOp false EvmYul.State.sstore symState := rfl
 
-/- This probably should be deleted -/
-theorem sstore_rw {symState : EVM.State} {key value : UInt256}:
-  ∃ Aᵣ: UInt256, State.sstore symState.toState key value =
-  let Iₐ := symState.toState.executionEnv.codeOwner
-  (Option.option symState.toState (λ acc ↦
-    let self' :=
-      symState.toState.setAccount Iₐ (Account.updateStorage acc key value)
-        |>.addAccessedStorageKey (Iₐ, key)
-    { self' with substate.refundBalance := Aᵣ}))
-    (State.lookupAccount symState.toState Iₐ):= by aesop
-
-theorem sstore_summary {symState : EVM.State} {key value : UInt256}:
-  ∃ Aᵣ: UInt256, State.sstore symState.toState key value =
+@[simp]
+def accountMap_sstore (symState : EVM.State) (key value : UInt256) : AccountMap :=
   let Iₐ := symState.toState.executionEnv.codeOwner
   let ownerAcc := (State.lookupAccount symState.toState Iₐ)
   match ownerAcc with
-  | none =>
-    {symState with
-      accountMap := symState.toState.accountMap
-      substate.accessedStorageKeys := symState.substate.accessedStorageKeys
-      substate.refundBalance := Aᵣ
-    }
+  | none => symState.toState.accountMap
   | some ownerAcc =>
+    symState.accountMap.insert Iₐ (Account.updateStorage (ownerAcc) key value)
+
+@[simp]
+def accessedStorageKeys_sstore (symState : EVM.State) (key : UInt256) :=
+  let Iₐ := symState.toState.executionEnv.codeOwner
+  let ownerAcc := (State.lookupAccount symState.toState Iₐ)
+  match ownerAcc with
+  | none => symState.substate.accessedStorageKeys
+  | some _ => symState.substate.accessedStorageKeys.insert ⟨Iₐ, key⟩
+
+theorem sstore_summary (symState : EVM.State) (key value : UInt256):
+  ∃ Aᵣ: UInt256, State.sstore symState.toState key value =
     {symState with
-      accountMap := symState.accountMap.insert Iₐ (Account.updateStorage (ownerAcc) key value)
-      substate.accessedStorageKeys := symState.substate.accessedStorageKeys.insert ⟨Iₐ, key⟩
+      accountMap := accountMap_sstore symState key value
+      substate.accessedStorageKeys := accessedStorageKeys_sstore symState key
       substate.refundBalance := Aᵣ
     } := by
   cases cco: (symState.lookupAccount symState.toState.executionEnv.codeOwner)
   all_goals aesop (add simp [sstore])
 
 theorem EvmYul.step_sstore_summary (symState : EVM.State):
+  ∃ Aᵣ: UInt256,
   EvmYul.step_sstore {symState with
     stack := key :: value :: symStack,
     pc := symPc} =
   .ok {symState with
     stack := symStack,
+    accountMap := accountMap_sstore symState key value
+    substate.accessedStorageKeys := accessedStorageKeys_sstore symState key
+    substate.refundBalance := Aᵣ
     pc := symPc + .ofNat 1} := by
   rw [sstore_bypass_private]
   simp [binaryStateOp, Stack.pop2, Id.run, State.replaceStackAndIncrPC, State.incrPC]
-  have sstoreRw := @sstore_rw symState key value
-  rcases sstoreRw; case intro gas ss_rw =>
-  simp [ss_rw]
-  rw [sstore_rw]
-  simp [State.addAccessedStorageKey, Account.updateStorage, State.setAccount]
-  cases value_default : (value == default) <;> simp
-  case false =>
-
-
-  simp [Batteries.RBMap.insert, Batteries.RBSet.insert, Batteries.RBNode.insert]
-  --simp [EvmYul.step, Id.run]
-  --simp [Id.run]
-  rw [step_sstore_rw]
-
+  have _ := sstore_summary symState key value; aesop
 
 end
 
