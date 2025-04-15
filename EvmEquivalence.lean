34f0896b78ff6ac89821c35622c9cfd5128b1863
Juan C <juanconrod@protonmail.com>
2025-04-15 19:23:20 +0200

    `EvmEquivalence.lean`: add modules that constitute the project

--- EvmEquivalence.lean
+++ EvmEquivalence.lean
@@ -1,3 +1,34 @@
 -- This module serves as the root of the `EvmEquivalence` library.
 -- Import modules here that should be built as part of the library.
-import EvmEquivalence.Basic
+
+-- K generated code
+import EvmEquivalence.KEVM2Lean.Prelude
+import EvmEquivalence.KEVM2Lean.Sorts
+import EvmEquivalence.KEVM2Lean.Inj
+import EvmEquivalence.KEVM2Lean.ScheduleOrdering
+import EvmEquivalence.KEVM2Lean.Func
+import EvmEquivalence.KEVM2Lean.Rewrite
+
+-- Interfaces
+import EvmEquivalence.Interfaces.EvmYulInterface
+import EvmEquivalence.Interfaces.FuncInterface
+import EvmEquivalence.Interfaces.Tactics
+import EvmEquivalence.Interfaces.GasInterface
+import EvmEquivalence.Interfaces.Axioms
+
+-- State Map
+import EvmEquivalence.StateMap
+
+-- Utils
+import EvmEquivalence.Utils.IntUtils
+
+-- Summaries
+import EvmEquivalence.Summaries.StopSummary
+import EvmEquivalence.Summaries.AddSummary
+import EvmEquivalence.Summaries.Push0Summary
+import EvmEquivalence.Summaries.SstoreSummary
+
+-- Equivalence
+import EvmEquivalence.Equivalence.AddEquivalence
+import EvmEquivalence.Equivalence.Push0Equivalence
+import EvmEquivalence.Equivalence.SstoreEquivalence
