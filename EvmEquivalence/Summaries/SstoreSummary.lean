f7296a2be24e08ec72b0db3d889ac56b6fdfc1f8
Juan C <juanconrod@protonmail.com>
2025-04-15 19:19:57 +0200

    `SstoreSummary`: complete proof of `EVM.step_sstore_summary`

--- EvmEquivalence/Summaries/SstoreSummary.lean
+++ EvmEquivalence/Summaries/SstoreSummary.lean
@@ -13,10 +13,12 @@ section
 
 variable (gas gasCost : ℕ)
 variable (symStack : Stack UInt256)
-variable (symPc : UInt256)
-variable (symGasAvailable key value : UInt256)
+variable (symPc symGasAvailable symRefund key value : UInt256)
 variable (symExecLength : ℕ)
 variable (symReturnData symCode : ByteArray)
+variable (symAccessedStorageKeys : Batteries.RBSet (AccountAddress × UInt256) Substate.storageKeysCmp)
+variable (symAccounts : AccountMap)
+variable (symCodeOwner : AccountAddress)
 
 @[simp]
 abbrev sstoreEVM := @Operation.SSTORE .EVM
@@ -26,61 +28,154 @@ abbrev sstore_instr : Option (Operation .EVM × Option (UInt256 × Nat)) :=
   some ⟨sstoreEVM, none⟩
 
 @[simp]
-def EVM.step_sstore : Transformer :=
+abbrev EVM.step_sstore : Transformer :=
   EVM.step false gas gasCost sstore_instr
 
 @[simp]
-def EvmYul.step_sstore : Transformer :=
+abbrev EvmYul.step_sstore : Transformer :=
   EvmYul.step false sstoreEVM
 
 /--
 Theorem needed to bypass the `private` attribute of `EVM.dispatchBinaryStateOp`
  -/
-theorem sstore_bypass_private {symState : EVM.State}:
-  EvmYul.step_sstore symState =
-  EVM.binaryStateOp false EvmYul.State.sstore symState := rfl
+theorem sstore_bypass_private (symState : EVM.State):
+  let ss := {symState with
+  stack := key :: value :: symStack,
+  pc := symPc
+  gasAvailable := symGasAvailable,
+  executionEnv := {symState.executionEnv with
+                  code := symCode,
+                  codeOwner := symCodeOwner},
+  substate := {symState.substate with
+               accessedStorageKeys :=  symAccessedStorageKeys
+               refundBalance := symRefund}
+  returnData := symReturnData,
+  execLength := symExecLength,
+  accountMap := symAccounts}
+  EvmYul.step_sstore ss =
+  EVM.binaryStateOp false EvmYul.State.sstore ss := rfl
 
 @[simp]
-def accountMap_sstore (symState : EVM.State) (key value : UInt256) : AccountMap :=
-  let Iₐ := symState.toState.executionEnv.codeOwner
-  let ownerAcc := (State.lookupAccount symState.toState Iₐ)
+def accountMap_sstore (symState : EvmYul.State) (key value : UInt256) : AccountMap :=
+  let Iₐ := symState.executionEnv.codeOwner
+  let ownerAcc := (State.lookupAccount symState Iₐ)
   match ownerAcc with
-  | none => symState.toState.accountMap
+  | none => symState.accountMap
   | some ownerAcc =>
     symState.accountMap.insert Iₐ (Account.updateStorage (ownerAcc) key value)
 
 @[simp]
-def accessedStorageKeys_sstore (symState : EVM.State) (key : UInt256) :=
-  let Iₐ := symState.toState.executionEnv.codeOwner
-  let ownerAcc := (State.lookupAccount symState.toState Iₐ)
+def accessedStorageKeys_sstore (symState : EvmYul.State) (key : UInt256) :=
+  let Iₐ := symState.executionEnv.codeOwner
+  let ownerAcc := (State.lookupAccount symState Iₐ)
   match ownerAcc with
   | none => symState.substate.accessedStorageKeys
   | some _ => symState.substate.accessedStorageKeys.insert ⟨Iₐ, key⟩
 
-theorem sstore_summary (symState : EVM.State) (key value : UInt256):
-  ∃ Aᵣ: UInt256, State.sstore symState.toState key value =
-    {symState with
-      accountMap := accountMap_sstore symState key value
-      substate.accessedStorageKeys := accessedStorageKeys_sstore symState key
-      substate.refundBalance := Aᵣ
-    } := by
-  cases cco: (symState.lookupAccount symState.toState.executionEnv.codeOwner)
-  all_goals aesop (add simp [sstore])
+-- From EvmYul.StateOps.sstore
+@[local simp]
+def Aᵣ_sstore (symState: EvmYul.State) (key val : UInt256) : UInt256 :=
+    let Iₐ := symState.executionEnv.codeOwner
+  let { storage := σ_Iₐ, .. } := symState.accountMap.find! Iₐ
+  let { storage := σ₀_Iₐ, .. } := symState.σ₀.find! Iₐ
+  let v₀ := σ₀_Iₐ.findD key ⟨0⟩
+  let v := σ_Iₐ.findD key ⟨0⟩
+  let v' := val
+
+  let r_dirtyclear : ℤ :=
+    if v₀ ≠ .ofNat 0 && v = .ofNat 0 then - GasConstants.Rsclear else
+    if v₀ ≠ .ofNat 0 && v' = .ofNat 0 then GasConstants.Rsclear else
+    0
+
+  let r_dirtyreset : ℤ :=
+    if v₀ = v' && v₀ = .ofNat 0 then GasConstants.Gsset - GasConstants.Gwarmaccess else
+    if v₀ = v' && v₀ ≠ .ofNat 0 then GasConstants.Gsreset - GasConstants.Gwarmaccess else
+    0
+
+  let ΔAᵣ : ℤ :=
+    if v ≠ v' && v₀ = v && v' = .ofNat 0 then GasConstants.Rsclear else
+    if v ≠ v' && v₀ ≠ v then r_dirtyclear + r_dirtyreset else
+    0
+
+  let Iₐ := symState.executionEnv.codeOwner
+  let ownerAcc := (State.lookupAccount symState Iₐ)
+  match ownerAcc with
+  | none => symState.substate.refundBalance
+  | some _ =>
+    match ΔAᵣ with
+        | .ofNat n => symState.substate.refundBalance + .ofNat n
+        | .negSucc n => symState.substate.refundBalance - .ofNat n - ⟨1⟩
+
+theorem sstore_summary (symState : EvmYul.State) (key value : UInt256):
+  let ss := {symState with
+             executionEnv := {symState.executionEnv with
+                  code := symCode,
+                  codeOwner := symCodeOwner},
+             substate := {symState.substate with
+                          accessedStorageKeys :=  symAccessedStorageKeys
+                          refundBalance := symRefund}
+             accountMap := symAccounts}
+  State.sstore ss key value =
+  {ss with
+    accountMap := accountMap_sstore ss key value,
+    substate.accessedStorageKeys := accessedStorageKeys_sstore ss key,
+    substate.refundBalance := Aᵣ_sstore ss key value} := by
+  cases cco: (symState.lookupAccount symState.executionEnv.codeOwner)
+  all_goals aesop (add simp [sstore, Aᵣ_sstore, Option.option, lookupAccount])
 
 theorem EvmYul.step_sstore_summary (symState : EVM.State):
-  ∃ Aᵣ: UInt256,
-  EvmYul.step_sstore {symState with
+  let ss := {symState with
     stack := key :: value :: symStack,
-    pc := symPc} =
-  .ok {symState with
+    pc := symPc
+    gasAvailable := symGasAvailable,
+    executionEnv := {symState.executionEnv with
+                  code := symCode,
+                  codeOwner := symCodeOwner},
+    substate := {symState.substate with
+                 accessedStorageKeys :=  symAccessedStorageKeys
+                 refundBalance := symRefund}
+    returnData := symReturnData,
+    execLength := symExecLength,
+    accountMap := symAccounts}
+  EvmYul.step_sstore ss =
+  .ok {ss with
     stack := symStack,
-    accountMap := accountMap_sstore symState key value
-    substate.accessedStorageKeys := accessedStorageKeys_sstore symState key
-    substate.refundBalance := Aᵣ
-    pc := symPc + .ofNat 1} := by
-  rw [sstore_bypass_private]
-  simp [binaryStateOp, Stack.pop2, Id.run, State.replaceStackAndIncrPC, State.incrPC]
-  have _ := sstore_summary symState key value; aesop
+    pc := symPc + .ofNat 1
+    accountMap := accountMap_sstore ss.toState key value
+    substate.accessedStorageKeys := accessedStorageKeys_sstore ss.toState key
+    substate.refundBalance := Aᵣ_sstore ss.toState key value} := by
+  simp [sstore_bypass_private, binaryStateOp, Stack.pop2, Id.run, State.replaceStackAndIncrPC, State.incrPC]
+  aesop (add simp [sstore_summary])
+
+theorem EVM.step_sstore_summary (gas_pos : 0 < gas) (symState : EVM.State):
+  let ss := {symState with
+    stack := key :: value :: symStack,
+    pc := symPc
+    gasAvailable := symGasAvailable,
+    executionEnv := {symState.executionEnv with
+                  code := symCode,
+                  codeOwner := symCodeOwner},
+    substate := {symState.substate with
+                 accessedStorageKeys :=  symAccessedStorageKeys
+                 refundBalance := symRefund}
+    returnData := symReturnData,
+    execLength := symExecLength,
+    accountMap := symAccounts}
+  EVM.step_sstore gas gasCost ss =
+    .ok {ss with
+          stack := symStack,
+          pc := symPc + (.ofNat 1),
+          gasAvailable := symGasAvailable - UInt256.ofNat gasCost,
+          accountMap := accountMap_sstore ss.toState key value
+          substate := {ss.substate with
+            accessedStorageKeys := accessedStorageKeys_sstore ss.toState key
+            refundBalance := Aᵣ_sstore ss.toState key value
+           }
+          execLength := symExecLength + 1} := by
+  cases gas; contradiction
+  simp [step_sstore, EVM.step]
+  have srw := EvmYul.step_sstore_summary symStack symPc (symGasAvailable - UInt256.ofNat gasCost) symRefund key value (symExecLength + 1) symReturnData symCode
+  simp [EvmYul.step_sstore, Operation.SSTORE] at srw; aesop
 
 end
 
