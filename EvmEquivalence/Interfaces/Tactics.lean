4547afdbac99e673962604d18261f90d84cf8a16
Juan C <juanconrod@protonmail.com>
2025-04-15 19:04:36 +0200

    `Tactics.lean`: add new tactic `simp_flag` for schedule flag reasoning

--- EvmEquivalence/Interfaces/Tactics.lean
+++ EvmEquivalence/Interfaces/Tactics.lean
@@ -5,8 +5,9 @@ This file contains tactics to agilize and compress certain repetitive proofs
 import EvmEquivalence.KEVM2Lean.Func
 
 -- Tactics to perform lengthy simps for schedule
-syntax "simp_schedule1"     : tactic
-syntax "simp_schedule2"     : tactic
+syntax "simp_schedule1" : tactic
+syntax "simp_schedule2" : tactic
+syntax "simp_flag"      : tactic
 
 -- The reason for having separate simps is that otherwise
 -- Lean performs too many operations at once
@@ -120,3 +121,78 @@ macro_rules
               (try simp [SCHEDULE_maxCodeSizeDragon]);
               (try simp [SCHEDULE_maxInitCodeSizeDefault]);
               (try simp [SCHEDULE_maxInitCodeSizeShanghai]))
+
+macro_rules
+| `(tactic| simp_flag) =>
+  `(tactic| (try simp [«_<<_>>_SCHEDULE_Bool_ScheduleFlag_Schedule»]);
+            (try simp [SCHEDULE_GemptyisnonexistentDefault]);
+            (try simp [SCHEDULE_GemptyisnonexistentDragon]);
+            (try simp [SCHEDULE_GhasaccesslistDefault]);
+            (try simp [SCHEDULE_GhasbasefeeDefault]);
+            (try simp [SCHEDULE_GhasbasefeeLondon]);
+            (try simp [SCHEDULE_GhasbeaconrootCancun]);
+            (try simp [SCHEDULE_GhasbeaconrootDefault]);
+            (try simp [SCHEDULE_GhasblobbasefeeCancun]);
+            (try simp [SCHEDULE_GhasblobbasefeeDefault]);
+            (try simp [SCHEDULE_GhasblobhashCancun]);
+            (try simp [SCHEDULE_GhasblobhashDefault]);
+            (try simp [SCHEDULE_GhaschainidDefault]);
+            (try simp [SCHEDULE_GhaschainidIstanbul]);
+            (try simp [SCHEDULE_Ghascreate2Constantinople]);
+            (try simp [SCHEDULE_Ghascreate2Default]);
+            (try simp [SCHEDULE_GhasdirtysstoreConstantinople]);
+            (try simp [SCHEDULE_GhasdirtysstoreDefault]);
+            (try simp [SCHEDULE_GhasdirtysstoreIstanbul]);
+            (try simp [SCHEDULE_GhasdirtysstorePetersburg]);
+            (try simp [SCHEDULE_Ghaseip6780Cancun]);
+            (try simp [SCHEDULE_Ghaseip6780Default]);
+            (try simp [SCHEDULE_GhasextcodehashConstantinople]);
+            (try simp [SCHEDULE_GhasextcodehashDefault]);
+            (try simp [SCHEDULE_GhasmaxinitcodesizeDefault]);
+            (try simp [SCHEDULE_GhasmaxinitcodesizeShanghai]);
+            (try simp [SCHEDULE_GhasmcopyCancun]);
+            (try simp [SCHEDULE_GhasmcopyDefault]);
+            (try simp [SCHEDULE_GhasprevrandaoDefault]);
+            (try simp [SCHEDULE_GhasprevrandaoMerge]);
+            (try simp [SCHEDULE_GhaspushzeroDefault]);
+            (try simp [SCHEDULE_GhaspushzeroShanghai]);
+            (try simp [SCHEDULE_GhasrejectedfirstbyteDefault]);
+            (try simp [SCHEDULE_GhasrejectedfirstbyteLondon]);
+            (try simp [SCHEDULE_GhasreturndataByzantium]);
+            (try simp [SCHEDULE_GhasreturndataDefault]);
+            (try simp [SCHEDULE_GhasrevertByzantium]);
+            (try simp [SCHEDULE_GhasrevertDefault]);
+            (try simp [SCHEDULE_GhasselfbalanceDefault]);
+            (try simp [SCHEDULE_GhasselfbalanceIstanbul]);
+            (try simp [SCHEDULE_GhasshiftConstantinople]);
+            (try simp [SCHEDULE_GhasshiftDefault]);
+            (try simp [SCHEDULE_GhassstorestipendDefault]);
+            (try simp [SCHEDULE_GhassstorestipendIstanbul]);
+            (try simp [SCHEDULE_GhasstaticcallByzantium]);
+            (try simp [SCHEDULE_GhasstaticcallDefault]);
+            (try simp [SCHEDULE_GhastransientCancun]);
+            (try simp [SCHEDULE_GhastransientDefault]);
+            (try simp [SCHEDULE_GhaswarmcoinbaseDefault]);
+            (try simp [SCHEDULE_GhaswarmcoinbaseShanghai]);
+            (try simp [SCHEDULE_GhaswithdrawalsDefault]);
+            (try simp [SCHEDULE_GhaswithdrawalsShanghai]);
+            (try simp [SCHEDULE_GselfdestructnewaccountDefault]);
+            (try simp [SCHEDULE_GselfdestructnewaccountTangerine]);
+            (try simp [SCHEDULE_GstaticcalldepthDefault]);
+            (try simp [SCHEDULE_GstaticcalldepthTangerine]);
+            (try simp [SCHEDULE_GzerovaluenewaccountgasDefault]);
+            (try simp [SCHEDULE_GzerovaluenewaccountgasDragon]);
+            (try simp [SCHEDULE_SCHEDFLAGBerlin]);
+            (try simp [SCHEDULE_SCHEDFLAGByzantium]);
+            (try simp [SCHEDULE_SCHEDFLAGCancun]);
+            (try simp [SCHEDULE_SCHEDFLAGConstantinople]);
+            (try simp [SCHEDULE_SCHEDFLAGDragon]);
+            (try simp [SCHEDULE_SCHEDFLAGFrontier]);
+            (try simp [SCHEDULE_SCHEDFLAGHomestead]);
+            (try simp [SCHEDULE_SCHEDFLAGIstanbul]);
+            (try simp [SCHEDULE_SCHEDFLAGLondon]);
+            (try simp [SCHEDULE_SCHEDFLAGMerge]);
+            (try simp [SCHEDULE_SCHEDFLAGPetersburg]);
+            (try simp [SCHEDULE_SCHEDFLAGShanghai]);
+            (try simp [SCHEDULE_SCHEDFLAGTangerine]);
+            (try simp [SCHEDULE_hasaccesslistBerlin]))
