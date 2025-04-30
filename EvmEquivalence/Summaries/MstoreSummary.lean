d21e02494ec15f712caa78e5c0e3e517d420bbc9
Juan C <juanconrod@protonmail.com>
2025-04-30 17:06:54 +0200

    `MstoreSummary`: prove `EvmYul` and `EVM` `step_mstore_summary`

--- EvmEquivalence/Summaries/MstoreSummary.lean
+++ EvmEquivalence/Summaries/MstoreSummary.lean
@@ -13,8 +13,9 @@ section
 variable (gas gasCost : ℕ)
 variable (symStack : Stack UInt256)
 variable (symPc symGasAvailable symRefund offset value : UInt256)
+variable (symActiveWords : UInt256)
 variable (symExecLength : ℕ)
-variable (symReturnData symCode : ByteArray)
+variable (symReturnData symCode symMemory : ByteArray)
 variable (symAccessedStorageKeys : Batteries.RBSet (AccountAddress × UInt256) Substate.storageKeysCmp)
 variable (symAccounts : AccountMap)
 variable (symCodeOwner : AccountAddress)
@@ -49,18 +50,31 @@ theorem mstore_bypass_private (symState : EVM.State):
                   code := symCode,
                   codeOwner := symCodeOwner
                   perm := symPerm},
+  accountMap := symAccounts,
+  activeWords := symActiveWords,
+  memory := symMemory,
   substate := {symState.substate with
                accessedStorageKeys :=  symAccessedStorageKeys
                refundBalance := symRefund}
   returnData := symReturnData,
-  execLength := symExecLength,
-  accountMap := symAccounts}
+  execLength := symExecLength}
   EvmYul.step_mstore ss =
   EVM.binaryMachineStateOp EvmYul.MachineState.mstore ss := rfl
 
+/--
+The new amount of `activeWords` based after running `MSTORE` with `offset`
+and `currentAC` amount of active words
+-/
+def mstore_activeWords :=
+  UInt256.ofNat (symActiveWords.toNat ⊔ (offset.toNat + 32 + 31) / 32)
 
+/--
+Writing `value` to `symMemory` starting at `offset`
+-/
+def mstore_memory_write :=
+value.toByteArray.write 0 symMemory offset.toNat 32
 
-theorem EvmYul.step_sstore_summary (symState : EVM.State):
+theorem EvmYul.step_mstore_summary (symState : EVM.State):
   let ss := {symState with
     stack := offset :: value :: symStack,
     pc := symPc
@@ -70,6 +84,8 @@ theorem EvmYul.step_sstore_summary (symState : EVM.State):
                   codeOwner := symCodeOwner
                   perm := symPerm},
     accountMap := symAccounts,
+    activeWords := symActiveWords,
+    memory := symMemory,
     substate := {symState.substate with
                  accessedStorageKeys :=  symAccessedStorageKeys
                  refundBalance := symRefund}
@@ -78,11 +94,40 @@ theorem EvmYul.step_sstore_summary (symState : EVM.State):
   EvmYul.step_mstore ss =
   .ok {ss with
     stack := symStack,
-    pc := symPc + .ofNat 1} := by
-  simp [mstore_bypass_private, binaryMachineStateOp, Stack.pop2, Id.run, State.replaceStackAndIncrPC, State.incrPC]
-  aesop (add simp [MachineState.mstore, MachineState.memory, MachineState.M])
-  simp [MachineState.writeWord, writeBytes]
-  -- aesop (add simp [mstore_summary])
+    pc := symPc + .ofNat 1
+    memory := mstore_memory_write offset value symMemory,
+    activeWords := mstore_activeWords offset symActiveWords} := by
+  aesop (add simp [mstore_bypass_private])
+
+theorem EVM.step_mstore_summary (gas_pos : 0 < gas) (symState : EVM.State):
+  let ss := {symState with
+    stack := offset :: value :: symStack,
+    pc := symPc
+    gasAvailable := symGasAvailable,
+    executionEnv := {symState.executionEnv with
+                  code := symCode,
+                  codeOwner := symCodeOwner
+                  perm := symPerm},
+    accountMap := symAccounts,
+    activeWords := symActiveWords,
+    memory := symMemory,
+    substate := {symState.substate with
+                 accessedStorageKeys :=  symAccessedStorageKeys
+                 refundBalance := symRefund}
+    returnData := symReturnData,
+    execLength := symExecLength}
+  EVM.step_mstore gas gasCost ss =
+    .ok {ss with
+          stack := symStack,
+          pc := symPc + (.ofNat 1),
+          gasAvailable := symGasAvailable - UInt256.ofNat gasCost,
+          memory := mstore_memory_write offset value symMemory,
+          activeWords := mstore_activeWords offset symActiveWords
+          execLength := symExecLength + 1} := by
+  cases gas; contradiction
+  simp [step_mstore, EVM.step]
+  have srw := EvmYul.step_mstore_summary symStack symPc (symGasAvailable - UInt256.ofNat gasCost) symRefund offset value symActiveWords (symExecLength + 1) symReturnData symCode symMemory
+  simp [EvmYul.step_mstore, Operation.MSTORE] at srw; aesop
 
 end
 
