791124d83a5b837236ebd950f74f34c655333926
Juan C <juanconrod@protonmail.com>
2025-06-23 13:51:06 +0200

    `OneOpEquivalence.lean`: proof obligations for `SLT` and `SGT`

--- EvmEquivalence/Equivalence/Arithmetic/OneOpEquivalence.lean
+++ EvmEquivalence/Equivalence/Arithmetic/OneOpEquivalence.lean
@@ -18,6 +18,8 @@ inductive arith_op where
   | mod
   | smod
   | signext
+  | slt
+  | sgt
 
 variable (op : arith_op)
 
@@ -28,6 +30,8 @@ def arith_op.to_binop : arith_op → SortBinStackOp
   | .mod  => .MOD_EVM_BinStackOp
   | .smod => .SMOD_EVM_BinStackOp
   | .signext => .SIGNEXTEND_EVM_BinStackOp
+  | .slt => .SLT_EVM_BinStackOp
+  | .sgt => .SGT_EVM_BinStackOp
 
 def arith_op.from_k : arith_op → ArithmeticSummary.arith_op
  | .div  => .div
@@ -35,6 +39,8 @@ def arith_op.from_k : arith_op → ArithmeticSummary.arith_op
  | .mod  => .mod
  | .smod => .smod
  | .signext => .signextend
+ | .slt => .slt
+ | .sgt => .sgt
 
 def oneOpLHS
   {GAS_CELL PC_CELL W0 W1 : SortInt}
@@ -181,6 +187,13 @@ def arith_op.to_defn_Val3 (W0 W1 _Val3 : SortInt) : Prop :=
   | .mod  => «_%Word__EVM-TYPES_Int_Int_Int» W0 W1  = some _Val3
   | .smod => «_%sWord__EVM-TYPES_Int_Int_Int» W0 W1 = some _Val3
   | .signext => «signextend» W0 W1 = some _Val3
+  | .slt => «_s<Word__EVM-TYPES_Int_Int_Int» W0 W1 = some _Val3
+  | .sgt => «_s<Word__EVM-TYPES_Int_Int_Int» W1 W0 = some _Val3
+
+@[simp]
+def arith_op.to_gas : arith_op → SortScheduleConst
+ | .slt | .sgt => .Gverylow_SCHEDULE_ScheduleConst
+ | _ => .Glow_SCHEDULE_ScheduleConst
 
 theorem rw_oneOpLHS_oneOpRHS
   {GAS_CELL PC_CELL W0 W1 _Val0 _Val3 _Val4 _Val5 _Val6 : SortInt}
@@ -214,12 +227,12 @@ theorem rw_oneOpLHS_oneOpRHS
   {_Gen8 : SortCallGasCell}
   {_Gen9 : SortStaticCell}
   {_K_CELL : SortK}
-  (defn_Val0 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» SortScheduleConst.Glow_SCHEDULE_ScheduleConst SCHEDULE_CELL = some _Val0)
+  (defn_Val0 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» op.to_gas SCHEDULE_CELL = some _Val0)
   (defn_Val1 : «_<=Int_» _Val0 GAS_CELL = some _Val1)
   (defn_Val2 : _andBool_ USEGAS_CELL _Val1 = some _Val2)
   (defn_Val3 : op.to_defn_Val3 W0 W1 _Val3)
   (defn_Val4 : «_+Int_» PC_CELL 1 = some _Val4)
-  (defn_Val5 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» SortScheduleConst.Glow_SCHEDULE_ScheduleConst SCHEDULE_CELL = some _Val5)
+  (defn_Val5 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» op.to_gas SCHEDULE_CELL = some _Val5)
   (defn_Val6 : «_-Int_» GAS_CELL _Val5 = some _Val6)
   (req : _Val2 = true):
   Rewrites
@@ -241,6 +254,10 @@ theorem rw_oneOpLHS_oneOpRHS
   . apply (@Rewrites.SMOD_SUMMARY_SMOD_SUMMARY_USEGAS GAS_CELL PC_CELL W0 W1 _Val0)
     <;> assumption
   . apply (@Rewrites.SIGNEXTEND_SUMMARY_SIGNEXTEND_SUMMARY_USEGAS GAS_CELL PC_CELL W0 W1 _Val0)
+    <;> assumption
+  . apply (@Rewrites.SLT_SUMMARY_SLT_SUMMARY_USEGAS GAS_CELL PC_CELL W0 W1 _Val0)
+    <;> assumption
+  . apply (@Rewrites.SGT_SUMMARY_SGT_SUMMARY_USEGAS GAS_CELL PC_CELL W0 W1 _Val0)
     <;> assumption
 
 theorem oneOp_prestate_equiv
@@ -318,6 +335,8 @@ def arith_op.do : SortInt → SortInt → SortInt :=
   | .mod  => (divWord · ·) -- Blatantly wrong
   | .smod => (divWord · ·) -- Blatantly wrong
   | .signext => (divWord · ·) -- Blatantly wrong
+  | .slt => (divWord · ·) -- Blatantly wrong
+  | .sgt => (divWord · ·) -- Blatantly wrong
 
 theorem oneOp_poststate_equiv
   {PC_CELL W0 W1 _Val3 _Val4 _Val5 _Val6 : SortInt}
@@ -397,6 +416,14 @@ theorem oneOp_poststate_equiv
       -- To prove this, first `arith_op.do` needs to be fixed for `signextend`
       aesop (add simp [«_-Int_»,«_+Int_», arith_op.to_defn_Val3, oneOpRHS, stateMap])
       sorry
+    . -- `slt`
+      -- To prove this, first `arith_op.do` needs to be fixed for `slt`
+      aesop (add simp [«_-Int_»,«_+Int_», arith_op.to_defn_Val3, oneOpRHS, stateMap])
+      sorry
+    . -- `sgt`
+      -- To prove this, first `arith_op.do` needs to be fixed for `sgt`
+      aesop (add simp [«_-Int_»,«_+Int_», arith_op.to_defn_Val3, oneOpRHS, stateMap])
+      sorry
 
 
 open ArithmeticSummary
@@ -408,6 +435,10 @@ def arith_op.gas :=
   | .mod  => GasConstants.Glow
   | .smod => GasConstants.Glow
   | .signext => GasConstants.Glow
+  | .slt => GasConstants.Gverylow
+  | .sgt => GasConstants.Gverylow
+
+attribute [local simp] GasConstants.Glow GasConstants.Gverylow
 
 -- We cannot prove full equivalence for the `EVM.step` function
 -- This is because it doesn't include all semantics such as gas computation
@@ -443,12 +474,12 @@ theorem step_oneOp_equiv
   {_Gen8 : SortCallGasCell}
   {_Gen9 : SortStaticCell}
   {_K_CELL : SortK}
-  (defn_Val0 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» SortScheduleConst.Glow_SCHEDULE_ScheduleConst SCHEDULE_CELL = some _Val0)
+  (defn_Val0 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» op.to_gas SCHEDULE_CELL = some _Val0)
   (defn_Val1 : «_<=Int_» _Val0 GAS_CELL = some _Val1)
   (defn_Val2 : _andBool_ USEGAS_CELL _Val1 = some _Val2)
   (defn_Val3 : op.to_defn_Val3 W0 W1 _Val3)
   (defn_Val4 : «_+Int_» PC_CELL 1 = some _Val4)
-  (defn_Val5 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» SortScheduleConst.Glow_SCHEDULE_ScheduleConst SCHEDULE_CELL = some _Val5)
+  (defn_Val5 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» op.to_gas SCHEDULE_CELL = some _Val5)
   (defn_Val6 : «_-Int_» GAS_CELL _Val5 = some _Val6)
   (req : _Val2 = true)
   (symState : EVM.State)
@@ -457,7 +488,7 @@ theorem step_oneOp_equiv
   -- Necessary assumptions for equivalence
   (cancun : SCHEDULE_CELL = .CANCUN_EVM)
   (gasEnough : 0 < gas)
-  (gavailEnough : 5 ≤ GAS_CELL)
+  (gavailEnough : op.gas ≤ GAS_CELL)
   (gavailSmall : GAS_CELL < ↑UInt256.size)
   (gasCostValue : gasCost = op.gas)
   (pcountSmall : PC_CELL + 1 < UInt256.size)
@@ -487,7 +518,9 @@ theorem step_oneOp_equiv
     rw [this]
     rw [EVM.step_add_summary] <;> try assumption
     simp [oneOpLHS, oneOpRHS]; constructor <;> try constructor
-    . aesop (add simp [arith_op.gas, GasConstants.Glow, GasInterface.cancun_def, «_-Int_», intMap_sub_dist])
+    . cases op <;>
+      simp [cancun, GasInterface.cancun_def] at defn_Val0 defn_Val5 <;>
+      aesop (add simp [arith_op.gas, GasInterface.cancun_def, «_-Int_», intMap_sub_dist])
     . rw [←UInt256.add_succ_mod_size, intMap_add_dist] <;> aesop
     . cases op <;> simp [arith_op.from_k]
       . -- `div` case
@@ -503,6 +536,12 @@ theorem step_oneOp_equiv
         sorry
       .  -- `signextend`
       -- To prove this, first `arith_op.do` needs to be fixed for `signextend`
+        sorry
+      .  -- `slt`
+      -- To prove this, first `arith_op.do` needs to be fixed for `slt`
+        sorry
+      .  -- `sgt`
+      -- To prove this, first `arith_op.do` needs to be fixed for `sgt`
         sorry
 
 attribute [local simp] GasConstants.Glow
@@ -547,12 +586,12 @@ theorem X_oneOp_equiv
   {_Gen8 : SortCallGasCell}
   {_Gen9 : SortStaticCell}
   {_K_CELL : SortK}
-  (defn_Val0 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» SortScheduleConst.Glow_SCHEDULE_ScheduleConst SCHEDULE_CELL = some _Val0)
+  (defn_Val0 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» op.to_gas SCHEDULE_CELL = some _Val0)
   (defn_Val1 : «_<=Int_» _Val0 GAS_CELL = some _Val1)
   (defn_Val2 : _andBool_ USEGAS_CELL _Val1 = some _Val2)
   (defn_Val3 : op.to_defn_Val3 W0 W1 _Val3)
   (defn_Val4 : «_+Int_» PC_CELL 1 = some _Val4)
-  (defn_Val5 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» SortScheduleConst.Glow_SCHEDULE_ScheduleConst SCHEDULE_CELL = some _Val5)
+  (defn_Val5 : «_<_>_SCHEDULE_Int_ScheduleConst_Schedule» op.to_gas SCHEDULE_CELL = some _Val5)
   (defn_Val6 : «_-Int_» GAS_CELL _Val5 = some _Val6)
   (req : _Val2 = true)
   (symState : EVM.State)
@@ -561,7 +600,7 @@ theorem X_oneOp_equiv
   (cancun : SCHEDULE_CELL = .CANCUN_EVM)
   (codeDiv : _Gen0 = ⟨op.from_k.to_bin⟩)
   (pcZero : PC_CELL = 0)
-  (enoughGas : 5 < GAS_CELL)
+  (enoughGas : op.gas < GAS_CELL)
   (boundedGas : GAS_CELL < ↑UInt256.size)
   (W0ge0 : 0 ≤ W0)
   (W1ge0 : 0 ≤ W1)
@@ -591,7 +630,9 @@ theorem X_oneOp_equiv
     op.from_k.stack (intMap W0) (intMap W1) (intMap W1) (wordStackMap WS) := by
       cases op <;> aesop
   rw [this, pc_equiv, X_arith_summary]
-  · cases op <;> simp [arith_op.from_k, arith_op.C'_comp, arith_op.C'_noexp]
+  · cases op <;>
+    simp [arith_op.from_k, arith_op.C'_comp, arith_op.C'_noexp] <;>
+    simp [cancun, GasInterface.cancun_def] at defn_Val0 defn_Val5
     . -- `div` case
       aesop (add simp [GasInterface.cancun_def, «_-Int_», chop_def, plusInt_def, intMap_add_dist, oneOpLHS, oneOpRHS])
       (add safe (by rw [intMap_sub_dist])) (add safe (by apply le_of_lt))
@@ -613,6 +654,16 @@ theorem X_oneOp_equiv
       sorry
     . -- `signextend`
       -- To prove this, first `arith_op.do` needs to be fixed for `signextend`
+      aesop (add simp [GasInterface.cancun_def, «_-Int_», chop_def, plusInt_def, intMap_add_dist, oneOpLHS, oneOpRHS])
+      (add safe (by rw [intMap_sub_dist])) (add safe (by apply le_of_lt))
+      sorry
+    . -- `slt`
+      -- To prove this, first `arith_op.do` needs to be fixed for `slt`
+      aesop (add simp [GasInterface.cancun_def, «_-Int_», chop_def, plusInt_def, intMap_add_dist, oneOpLHS, oneOpRHS])
+      (add safe (by rw [intMap_sub_dist])) (add safe (by apply le_of_lt))
+      sorry
+    . -- `sgt`
+      -- To prove this, first `arith_op.do` needs to be fixed for `sgt`
       aesop (add simp [GasInterface.cancun_def, «_-Int_», chop_def, plusInt_def, intMap_add_dist, oneOpLHS, oneOpRHS])
       (add safe (by rw [intMap_sub_dist])) (add safe (by apply le_of_lt))
       sorry
